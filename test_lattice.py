2720d772a4ff657cf3f58a9d6c652abfdddfdcbc
Kannav <kmkannavkmehta@gmail.com>
2023-04-17 19:15:15 +0530

    add randbasis and extbasis

--- test_lattice.py
+++ test_lattice.py
@@ -1,7 +1,7 @@
 import numpy as np
-from lattice import HNF
+from bonsai import HNF
 
 def test_HNF():
-	S = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-	H = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+	S = np.array([[12, 6, 4], [3, 9, 6], [2, 16, 14]])
+	H = np.array([[10, 0, 2], [0, 15, 3], [0, 0, 2]])
 	assert np.allclose(HNF(S), H)
