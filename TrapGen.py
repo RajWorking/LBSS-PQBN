2616b36adb664abfcde8e0048ed8dd4209a0a851
RajWorking <rajmaheshwari08@yahoo.com>
2023-04-19 23:40:00 +0530

    Construction of P Matrix

--- TrapGen.py
+++ TrapGen.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy as sc
 from config import *
 
-# np.set_printoptions(threshold=np.inf, linewidth=100000)
+np.set_printoptions(threshold=np.inf, linewidth=100000)
 
 
 def TrapGen():
@@ -24,34 +24,42 @@ def Algorithm_1(A1):
         S (np.ndarray): integer (modulo q) matrix of dimensions m x m
     '''
     # TODO: compute H
-    H = A1[:m1, :m1]  # HNF(A1)
+    # H = (A1.T @ A1) % q  # HNF(A1)
+    H = np.random.choice([1, 2], (m1, m1), [0.75, 0.25])
 
-    # Construction of G
-    G = np.zeros((m1, m2))
-    widths = np.ceil(np.log2(np.diagonal(H))).astype(int)
-
-    col = 0
-    e = np.identity(m1)
-    for i, width in enumerate(widths):
-        G[:, col: col+width] = e[i, None].T @ [2 ** np.arange(0, width)]
-        col += width
-
-    w = 2 ** math.floor(math.log2(m2 - 2 * n * math.log2(q)))
-    C_ = 1e4
-    G[:, col: col+w] = sc.linalg.hadamard(w)[:m1]
-    
-    # Construction of R
-    R = np.random.choice([-1, 0, 1], (m1, m2), [0.25, 0.5, 0.25])
-    
     # Construction of C
     C = np.identity(m1).astype(int)
 
+    # Construction of R
+    R = np.random.choice([-1, 0, 1], (m1, m2), [0.25, 0.5, 0.25])
+
+    # Construction of G
+    G = np.zeros((m1, m2), dtype=np.int64)
+    widths = np.ceil(np.log2(np.diagonal(H))).astype(int)
+
+    col = 0
+    for i, width in enumerate(widths):
+        G[:, col: col+width] = C[i, None].T @ [2 ** np.arange(0, width)]
+        col += width
+
+    w = 2 ** math.floor(math.log2(m2 - 2 * n * math.log2(q)))
+    C_ = 1000
+    G[:, col: col+w] = C_ * sc.linalg.hadamard(w)[:m1]
+
     # Construction of P
-    P = np.zeros((m2, m1))
+    P = np.zeros((m2, m1), dtype=np.int64)
+    H_ = H - C
+
+    row = 0
+    for i, width in enumerate(widths):
+        P[row: row+width] = ((H_[i][None].T &
+                             (2**np.arange(width))) > 0).T.astype(int)
+        row += width
+
+    print(G@P - H_)
 
     # Construction of U
     U = np.zeros((m2, m2))
-
 
     A2 = (-(A1 @ (R + G))) % q
 
